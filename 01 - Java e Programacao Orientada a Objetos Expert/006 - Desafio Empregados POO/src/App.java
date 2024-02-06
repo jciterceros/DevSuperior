478468e2a805069cab008d7c72e8f0f63dff2cbf
jciterceros <jciterceros@yahoo.com>
2024-02-05 23:33:55 -0300

    add solution Deafio Empregados POO Java

--- 01 - Java e Programacao Orientada a Objetos Expert/006 - Desafio Empregados POO/src/App.java
+++ 01 - Java e Programacao Orientada a Objetos Expert/006 - Desafio Empregados POO/src/App.java
@@ -1,17 +1,63 @@
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Department;
+import entities.Employee;
+import entities.Address;
+
 public class App {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        // Department dept = new ...
-        // ...
+
+        System.out.print("Nome do departamento: ");
+        String deptName = sc.nextLine();
+
+        System.out.print("Dia do pagamento: ");
+        int payDay = sc.nextInt();
+        sc.nextLine(); // Consumir a quebra de linha após a leitura do inteiro
+
+        System.out.print("Email: ");
+        String deptEmail = sc.nextLine();
+
+        System.out.print("Telefone: ");
+        String deptPhone = sc.nextLine();
+
+        Address deptAddress = new Address(deptEmail, deptPhone);
+        Department dept = new Department(deptName, payDay, deptAddress);
+
+        System.out.print("Quantos funcionários tem o departamento? ");
+        int numEmployees = sc.nextInt();
+        sc.nextLine(); // Consumir a quebra de linha após a leitura do inteiro
+
+        for (int i = 1; i <= numEmployees; i++) {
+            System.out.println("Dados do funcionário " + i + ":");
+            System.out.print("Nome: ");
+            String empName = sc.nextLine();
+
+            System.out.print("Salário: ");
+            double empSalary = sc.nextDouble();
+            sc.nextLine(); // Consumir a quebra de linha após a leitura do double
+
+            Employee employee = new Employee(empName, empSalary);
+            dept.addEmployee(employee);
+        }
+
         showReport(dept);
         sc.close();
     }
 
     private static void showReport(Department dept) {
-        // MOSTRAR NA TELA OS DADOS DO RELATÓRIO
+        System.out.println("");
+        System.out.println("FOLHA DE PAGAMENTO:");
+        System.out.println("Departamento " + dept.getName() + " = R$ " + dept.payroll());
+        System.out.println("Pagamento realizado no dia " + dept.getPayDay());
+        System.out.println("Funcionários:");
+
+        for (Employee employee : dept.getEmployees()) {
+            System.out.println(employee.getName());
+        }
+
+        System.out.println("Para dúvidas, favor entrar em contato: " + dept.getEmail());
     }
 }
---
+++ 01 - Java e Programacao Orientada a Objetos Expert/006 - Desafio Empregados POO/src/entities/Employee.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Employee {
+    private String name;
+    private double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
