18d911e388855ba6c520b07ec89828c6182637ed
jciterceros <jciterceros@yahoo.com>
2024-02-05 21:50:20 -0300

    solution for DESAFIO Contribuintes OO e Lista Java

--- 01 - Java e Programacao Orientada a Objetos Expert/005 - Desafio Contribuintes POO e Lista Java/src/App.java
+++ 01 - Java e Programacao Orientada a Objetos Expert/005 - Desafio Contribuintes POO e Lista Java/src/App.java
@@ -1,11 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+
+import entities.TaxPayer;
 
 public class App {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Quantos contribuintes você vai digitar? ");
+        int n = sc.nextInt();
+        List<TaxPayer> taxpayers = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("");
+            System.out.println("Digite os dados do " + i + "o contribuinte:");
+            System.out.print("Renda anual com salário: ");
+            double salaryIncome = sc.nextDouble();
+            System.out.print("Renda anual com prestação de serviço: ");
+            double servicesIncome = sc.nextDouble();
+            System.out.print("Renda anual com ganho de capital: ");
+            double capitalIncome = sc.nextDouble();
+            System.out.print("Gastos médicos: ");
+            double healthSpending = sc.nextDouble();
+            System.out.print("Gastos educacionais: ");
+            double educationSpending = sc.nextDouble();
+            TaxPayer taxpayer = new TaxPayer(salaryIncome, servicesIncome, capitalIncome, healthSpending,
+                    educationSpending);
+            taxpayers.add(taxpayer);
+        }
+        for (int i = 0; i < taxpayers.size(); i++) {
+            TaxPayer taxpayer = taxpayers.get(i);
+            System.out.println();
+            System.out.println("Resumo do " + (i + 1) + "o contribuinte:");
+            System.out.printf("Imposto bruto total: %.2f%n", taxpayer.grossTax());
+            System.out.printf("Abatimento:  %.2f%n", taxpayer.taxRebate());
+            System.out.printf("Imposto devido:  %.2f%n", taxpayer.netTax());
+        }
         sc.close();
     }
 }
--- 01 - Java e Programacao Orientada a Objetos Expert/005 - Desafio Contribuintes POO e Lista Java/src/entities/TaxPayer.java
+++ 01 - Java e Programacao Orientada a Objetos Expert/005 - Desafio Contribuintes POO e Lista Java/src/entities/TaxPayer.java
@@ -1,5 +1,53 @@
 package entities;
 
 public class TaxPayer {
+    private double salaryIncome;
+    private double servicesIncome;
+    private double capitalIncome;
+    private double healthSpending;
+    private double educationSpending;
+
+    public TaxPayer(double salaryIncome, double servicesIncome, double capitalIncome, double healthSpending,
+            double educationSpending) {
+        this.salaryIncome = salaryIncome;
+        this.servicesIncome = servicesIncome;
+        this.capitalIncome = capitalIncome;
+        this.healthSpending = healthSpending;
+        this.educationSpending = educationSpending;
+    }
+
+    public double salaryTax() {
+        double monthlySalary = salaryIncome / 12;
+        if (monthlySalary < 3000) {
+            return 0.0;
+        } else if (monthlySalary < 5000) {
+            return (monthlySalary * 0.1) * 12;
+        } else {
+            return (monthlySalary * 0.2) * 12;
+        }
+    }
+
+    public double servicesTax() {
+        return (servicesIncome > 0) ? servicesIncome * 0.15 : 0.0;
+    }
+
+    public double capitalTax() {
+        return (capitalIncome > 0) ? capitalIncome * 0.2 : 0.0;
+    }
+
+    public double grossTax() {
+        return salaryTax() + servicesTax() + capitalTax();
+    }
+
+    public double taxRebate() {
+        double deductibleExpenses = (healthSpending > 0 ? healthSpending : 0)
+                + (educationSpending > 0 ? educationSpending : 0);
+        double maxDeductible = grossTax() * 0.3;
+        return Math.min(deductibleExpenses, maxDeductible);
+    }
+
+    public double netTax() {
+        return grossTax() - taxRebate();
+    }
 
 }
